7b03810bf802bc1ea8f56ca74ad5033da8049c43
Cosmin Stoian <cosmyn9708@gmail.com>
2020-12-05 15:31:51 +0200

    Initial commit. Implemented a script that reads system temperature

---
+++ system/monitoring/my-pi-temp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: Cosmin Stoian
+# ----------------------------------
+
+cpu=$(</sys/class/thermal/thermal_zone0/temp)
+echo "$(date) @ $(hostname)"
+echo "------------------------"
+echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
+echo "CPU => temp=$((cpu/1000))'C"
